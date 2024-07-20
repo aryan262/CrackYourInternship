405d360898fed9f24fcb0af473a63ef6ae8b1144
Aryan Panchal <97938438+aryan262@users.noreply.github.com>
2024-07-20 21:28:46 +0530

    Day  4

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        int minLen = INT_MAX;
+        for (string str:strs) minLen = min(minLen, (int)str.length());
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low+high) / 2;
+            if (common(strs, middle))
+                low = middle+1;
+            else
+                high = middle-1;
+        }
+        return strs[0].substr(0, (low+high)/2);
+    }
+private:
+    bool common(vector<string>& strs, int len) {
+        string str1=strs[0].substr(0, len);
+        for (int i=1;i<strs.size();i++)
+            if (strs[i].find(str1) != 0)return false;
+        return true;
+    }    
+};
